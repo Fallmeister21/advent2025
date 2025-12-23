06b8b93be508482001ed04b9c7e712323b944ff9
Paul Hoffer <phoffer@eriestrayer.com>
2025-12-23 16:58:44 -0500

    Day 7 part 2

--- day7/main.cpp
+++ day7/main.cpp
@@ -1,18 +1,23 @@
 #include "../fileOpener.cpp"
 #include <set>
+#include <map>
 
 std::pair<int, int> GetManifoldPos(std::vector<std::string> fileInput);
 int GetSplitterPositions(std::vector<std::string> fileInput);
+long long GetAllTimelines(std::vector<std::string> fileInput);
+long long CheckBeamTimelines(std::vector<std::string> fileInput, int i, int j, std::map<std::pair<int, int>, long long> &beamPaths);
 
 int main()
 {
 	std::vector<std::string> fileInput = openFile("day7Input");
 	std::pair<int, int> manifoldPos =  GetManifoldPos(fileInput);
 	int splitterPositions = GetSplitterPositions(fileInput);
+	long long splitterTimeLines = GetAllTimelines(fileInput);
 
 	std::cout << "Manifold position: " << manifoldPos.first << "," << manifoldPos.second << std::endl;
 
 	std::cout << "Splitter size: " << splitterPositions << std::endl;
+	std::cout << "All beam timelines: " << splitterTimeLines << std::endl;
 
 	return 0;
 }
@@ -64,4 +69,36 @@ int GetSplitterPositions(std::vector<std::string> fileInput)
 
 	std::cout << beamSplits << std::endl;
 	return beamSplits;
+}
+
+long long GetAllTimelines(std::vector<std::string> fileInput)
+{
+	std::map<std::pair<int, int>, long long> beamPaths;
+	long long totalNumberOfTimelines = 0;
+
+	for (int i = 0; i < fileInput[0].length(); i++) {
+		char c = fileInput[0][i];
+		if (c == 'S') {
+			totalNumberOfTimelines = CheckBeamTimelines(fileInput, 0, i, beamPaths);
+			break;
+		}
+	}
+
+	return totalNumberOfTimelines;
+}
+
+long long CheckBeamTimelines(std::vector<std::string> fileInput, int i, int j, std::map<std::pair<int, int>, long long> &beamPaths)
+{
+	if (i == fileInput.size() - 1) {
+		return 1;
+	}
+	if (fileInput[i][j] == '^') {
+		if (beamPaths.count({i, j})) {
+			return beamPaths.at({i, j});
+		}
+		long long traces = CheckBeamTimelines(fileInput, i + 1, j - 1, beamPaths) + CheckBeamTimelines(fileInput, i + 1, j + 1, beamPaths);
+		beamPaths[{i, j}] = traces;
+		return traces;
+	}
+	return CheckBeamTimelines(fileInput, i + 1, j, beamPaths);
 }
